e95c64b5db78219efa8fc1bcce714d012ec56449
Jadon Thomson <jadon.thomson@zestlife.co.za>
2016-01-22 12:33:43 +0200

    changed some stuff

--- Hello_World.git/Hello_World.R
+++ Hello_World.git/Hello_World.R
@@ -7,3 +7,9 @@ len <- nchar(HW)
 for (i in 1:len){
   print(as.character(substr(HW, i, i)))     #splitting up the characters
 }
+
+#reverse text
+WH <- ""
+for (i in 1:len){
+  WH <- paste0(WH, substr(HW, len + 1 - i, len + 1 - i))
+}
